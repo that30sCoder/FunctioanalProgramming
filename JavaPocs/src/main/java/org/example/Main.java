3732e0186bac88ae6deb39ab2f6f6527847b109d
that30sCoder <rahulksingh1092@gmail.com>
2024-05-04 20:50:44 +0530

    java 17 streams

---
+++ JavaPocs/src/main/java/org/example/Main.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+class Main {
+    public static void main(String[] args) throws InterruptedException{
+
+       /* Thread t1 = new Thread(new PrintThreads(Boolean.TRUE));
+        t1.setName("EvenThread");
+        Thread t2 = new Thread(new PrintThreads(Boolean.FALSE));
+        t2.setName("OddThread");
+        t2.start();
+        t1.start();
+
+        */
+
+        List<Employee> employeeList = new ArrayList<Employee>();
+
+        employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
+        employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+        employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
+        employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
+        employeeList.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
+        employeeList.add(new Employee(166, "Iqbal Hussain", 43, "Male", "Security And Transport", 2016, 10500.0));
+        employeeList.add(new Employee(177, "Manu Sharma", 35, "Male", "Account And Finance", 2010, 27000.0));
+        employeeList.add(new Employee(188, "Wang Liu", 31, "Male", "Product Development", 2015, 34500.0));
+        employeeList.add(new Employee(199, "Amelia Zoe", 24, "Female", "Sales And Marketing", 2016, 11500.0));
+        employeeList.add(new Employee(200, "Jaden Dough", 38, "Male", "Security And Transport", 2015, 11000.5));
+        employeeList.add(new Employee(211, "Jasna Kaur", 27, "Female", "Infrastructure", 2014, 15700.0));
+        employeeList.add(new Employee(222, "Nitin Joshi", 25, "Male", "Product Development", 2016, 28200.0));
+        employeeList.add(new Employee(233, "Jyothi Reddy", 27, "Female", "Account And Finance", 2013, 21300.0));
+        employeeList.add(new Employee(244, "Nicolus Den", 24, "Male", "Sales And Marketing", 2017, 10700.5));
+        employeeList.add(new Employee(255, "Ali Baig", 23, "Male", "Infrastructure", 2018, 12700.0));
+        employeeList.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
+        employeeList.add(new Employee(277, "Anuj Chettiar", 31, "Male", "Product Development", 2012, 35700.0));
+       /* System.out.printf("Number of male and female employees" +
+                employeeList.stream().collect(Collectors.groupingBy(employee -> employee.getGender(),Collectors.counting()))
+                );
+
+        */
+
+/*        System.out.printf("All departments are" + employeeList.stream().map(Employee::getDepartment).collect(Collectors.toList()));
+
+ */
+       /* System.out.printf("Average age of male and female"
+        + employeeList.stream().collect(Collectors.groupingBy(employee -> employee.getGender(),Collectors.averagingInt(employee->employee.getAge())))
+        );
+
+        */
+
+        /*System.out.printf("Highest paid" +
+                employeeList.stream().collect(Collectors.maxBy(Comparator.comparingDouble(employee->employee.getSalary()))));
+
+         */
+
+    }
+
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s)
+    {
+        // Your code here
+        ArrayList<Integer> indexList = new ArrayList<>();
+
+        for(int i = 0;i<arr.length-1;i++){
+            if((arr[i]+ arr[i+1])==s){
+                indexList.add(i);
+                indexList.add(i+1);
+                break;
+            }
+        }
+        return indexList;
+    }
+
+
+
+}
